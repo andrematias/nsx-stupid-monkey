599666e230b76af78069a603d5208ca87273804e
André Matias <dev.andrematias@gmail.com>
2018-10-14 03:04:49 -0300

    Fazendo o macaco pensar sobre os dados e tomar uma atitude

--- monkey.py
+++ monkey.py
@@ -1,13 +1,38 @@
 import settings
 import report
+import rules
+import database
+import webbrowser
 
 
-def main():
-    if report.make_invoices_report(settings.INVOICE_REPORT_PATH):
-        return 'Relatório criado com sucesso em %s' % settings.INVOICE_REPORT_PATH
-    else:
-        return 'Falha ao criar o relatório'
+def alert():
+    print("Analisando os dados de envio de faturas NET Atende...")
+    try:
+        invoices_queued = database.qtd_invoices()[1::][0][0]
+        rules.max_invoices_queued(invoices_queued)
+
+        last_invoice = database.last_invoice()[1::]
+        rules.time_diff(last_invoice)
+
+        last_invoices = database.last_send_mails()[1::]
+        rules.diff_request_and_send_mail(last_invoices)
+
+        print('Até agora tudo certo, Ua a a!!! :D')
+        webbrowser.open(settings.SUCCESS_ALARM_PATH)
+
+    except rules.MaxInvoiceQueuedException as max_queue:
+        print(max_queue)
+        webbrowser.open(settings.DANGER_ALARM_PATH)
+
+    except rules.SendMailTimeException as latence:
+        print(latence)
+        webbrowser.open(settings.DANGER_ALARM_PATH)
+
+
+def job():
+    alert()
+    report.main()
 
 
 if __name__ == '__main__':
-    print(main())
+    job()
